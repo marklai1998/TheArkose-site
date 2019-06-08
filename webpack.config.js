e44b162373359c1ffbade805a47ecc2b86cfeb3a
Mark Lai <marklai1998@gmail.com>
2019-06-08 20:12:33 +0800

    chore: update dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = env => ({
   entry: ['@babel/polyfill', './src/index.js'],
